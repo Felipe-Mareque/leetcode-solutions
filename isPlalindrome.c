c7ccc2ed78fc646bf6563f158e568778c5b127f3
Felipe Mareque <felipemareque@gmail.com>
2026-04-16 09:32:25 -0300

    palindromo

---
+++ isPlalindrome.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isPalindrome(char* s);
+
+int main(){
+    char s[20];
+    bool resultado;
+    printf("diga seu numero;");
+    scanf("%d",s);
+
+    resultado = isPalindrome(s);
+}
+
+bool isPalindrome(char* s){
+
+    
+
+}
